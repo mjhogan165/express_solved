268597dffdec42065e3ef9f3f324da375269a258
Matthew Hogan <69777851+mjhogan165@users.noreply.github.com>
2023-11-12 23:19:37 -0500

    dev.db

--- src/app.ts
+++ src/app.ts
@@ -17,137 +17,161 @@ app.get("/dogs", async (req, res) => {
   res.status(200).send(dogs);
 });
 
-app.get("/dogs/:id", async (req, res) => {
+app.get("/dogs/:id", async (req, res, next) => {
   const id = +req.params.id;
   if (!Number.isInteger(id)) {
-    res.status(400).send("id should be a number");
+    res
+      .status(400)
+      .send({ message: "id should be a number" });
   }
-  // console.log(id);
-  // console.log(req.params);
-  return await prisma.dog
+  await prisma.dog
     .findMany({
       where: {
         id: id,
       },
     })
-    .then((result: any) => {
-      console.log(result);
-      return result;
-      // if (result === null) {
-      //   res.status(204).send("not found");
-      // }
-    });
+    .then((x) => {
+      if (x.length === 0) {
+        res.status(204).send();
+      } else res.send(x);
+    })
+    .catch((err) => next(err));
 });
-
-function testInput<T>(body: T): string[] {
-  const errors: string[] = [];
-  const requiredFields = [
-    ["age", "number"],
-    ["name", "string"],
-    ["description", "string"],
-    ["breed", "string"],
-  ];
-  const reqProps = ["age", "name", "description", "breed"];
-  if (typeof body !== "object" || body === null) {
-    return ["invalid input"];
-  } else {
-    const inputFields: any = Object.entries(body);
-    const inputProps = Object.keys(body);
-    for (const reqField of requiredFields) {
-      for (const inputField of inputFields) {
-        console.log(inputField);
-        if (inputField[0] === reqField[0]) {
-          switch (inputField[0]) {
-            case "age":
-              if (typeof inputField[1] !== "number") {
-                errors.push(
-                  String.raw`${inputField[0]} should be a numberc`
-                );
-              }
-              break;
-            case "description":
-              if (typeof inputField[1] !== "string") {
-                errors.push(
-                  String.raw`${inputField[0]} should be a string`
-                );
-              }
-              break;
-            case "breed":
-              if (typeof inputField[1] !== "string") {
-                errors.push(
-                  String.raw`${inputField[0]} should be a string`
-                );
-              }
-              break;
-            case "name":
-              if (typeof inputField[1] !== "string") {
-                errors.push(
-                  String.raw`${inputField[0]} should be a string`
-                );
-              }
-              break;
-            default:
-              console.log(`default on ${inputField}`);
-              errors.push(
-                String.raw`${inputField[0]} is not a valid key`
-              );
-              break;
-          }
-        } else if (
-          !reqProps.includes(inputField[0]) &&
-          !errors.includes(
-            String.raw`${inputField[0]} is not a valid key`
-          )
-        ) {
-          errors.push(
-            String.raw`${inputField[0]} is not a valid key`
-          );
-        }
-      }
-    }
-    if (inputFields.length < requiredFields.length) {
-      for (
-        let index = 0;
-        index < requiredFields.length;
-        index++
-      ) {
-        let missing;
-        const current = requiredFields[index];
-        const key = current[0];
-        const check = inputProps.find((x) => x === key);
-        if (!check) {
-          missing = key;
-          if (missing && missing === "age") {
-            errors.push(
-              String.raw`${missing} should be a number`
-            );
-          } else if (missing) {
-            errors.push(
-              String.raw`${missing} should be a string`
-            );
-          }
-        }
-      }
-    }
-    return errors;
-  }
-}
 
 app.post("/dogs", async (req, res) => {
   const body = req.body;
-  const errorCheck = testInput(req.body);
-  const age = parseInt(body.age);
-  if (errorCheck.length < 1) {
+  console.log(req.body);
+  // const errorCheck = testInput(req.body);
+  // // const age = parseInt(body.age);
+  // console.log({ age: age });
+  const errors: string[] = [];
+  let name = req?.body?.name;
+  let age = req?.body?.age;
+  let description = req?.body?.description;
+  let breed = req?.body?.breed;
+
+  if (typeof name !== "string") {
+    name = "";
+    console.log({ typename: typeof name });
+    errors.push("name should be a string");
+  }
+  if (typeof age !== "number") {
+    age = "";
+    errors.push("age should be a number");
+  }
+  if (typeof description !== "string") {
+    description = "";
+    errors.push("description should be a string");
+  }
+  if (typeof breed !== "string") {
+    breed = "";
+    errors.push("breed should be a string");
+  }
+  const reqKeys = ["name", "age", "description", "breed"];
+  const keys = Object.keys(body);
+  for (const inputKey of keys) {
+    if (!reqKeys.find((key) => key === inputKey)) {
+      errors.push(`${inputKey} is not a valid key`);
+    }
+  }
+  console.log({
+    name: name,
+    age: age,
+    breed: breed,
+    description: description,
+  });
+  console.log({ errors: errors, length: errors.length });
+  if (errors.length === 0) {
+    console.log("creat");
     await prisma.dog.create({
       data: {
         name: body.name,
-        age: age,
+        age: body.age,
         description: body.description,
         breed: body.breed,
       },
     });
     return res.status(201).send("dog created");
-  } else return res.status(400).json(errorCheck);
+  } else return res.status(400).send(errors);
+});
+
+app.patch("/dogs/:id", async (req, res) => {
+  const id = +req.params.id;
+  const name = req?.body?.name;
+  const age = req?.body?.age;
+  const description = req?.body?.description;
+  const breed = req?.body?.breed;
+  const errors = [];
+  // type update = {
+  //   na
+  // };
+  type UpdateType = {
+    name?: string;
+    age?: number;
+    description?: string;
+    breed?: string;
+  };
+  const update: UpdateType = {};
+
+  const validKeys = Object.keys(update);
+  const inputKeys = Object.keys(req.body);
+  for (const key of inputKeys) {
+    if (!validKeys.includes(key)) {
+      errors.push(key);
+      res.status(400).send(`${key} is not a valid key`);
+    }
+  }
+
+  if (name) {
+    update.name = name;
+  }
+  if (age) {
+    update.age = age;
+  }
+  if (description) {
+    update.description = description;
+  }
+  if (breed) {
+    update.breed = breed;
+  }
+  await prisma.dog
+    .update({
+      where: { id: id },
+      data: update,
+    })
+    .then((x) => {
+      if (errors.length === 0 && x) {
+        res.status(201).send("dog");
+      } else res.status(400).send("no dog");
+    })
+    .catch((err) => err);
+});
+
+app.delete("/dogs/:id", async (req, res, next) => {
+  const id = req.params.id;
+  const errors = [];
+  console.log({ id: id });
+  if (typeof id !== "number") {
+    errors.push("id must be a number");
+  }
+  const dog = await prisma.dog
+    .delete({
+      where: {
+        id: +req.params.id,
+      },
+    })
+    .then((value) => {
+      console.log({ val: value });
+      if (errors.length === 0) {
+        res.status(201).send("dog deleted");
+      }
+
+      // res.status(201).send("done");
+    })
+    .catch((err) => {
+      // next(err);
+      res.status(400).json({ message: "dog not found" });
+    });
 });
 
 // all your code should go above this line
